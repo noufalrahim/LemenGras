e570713eca94865edbd4cc8873a330f7bae0568c
noufalrahim <noufalrahim6784@gmail.com>
2023-07-28 15:36:59 +0530

    first commit

--- frontend/src/components/UI/Card/Card.js
+++ frontend/src/components/UI/Card/Card.js
@@ -1,5 +1,5 @@
 import ItemContext from "../../store/Item-context";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import React from "react";
 import Cart from "../../Cart/Cart";
@@ -13,7 +13,7 @@ function Card() {
       <Cart />
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <h2 className="text-5xl">PRODUCT</h2>
+          <h2 className="text-5xl">SHOP WITH US</h2>
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
             {itemCtx.map((item) => (
               <Link to={`/${item["id"]}/productdetails`} className="group">
